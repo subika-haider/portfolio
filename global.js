42eb8e71ca3a03cebd180be577e2dda0c4f67d19
suhaider <suhaider@ucsd.edu>
2025-05-03 20:13:47 -0700

    debug attempt 1

--- global.js
+++ global.js
@@ -25,7 +25,7 @@ let pages = [
   { url: 'project2/', title: 'Project II'}
 ];
 
-const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+export const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"          
     : "/portfolio/"; 
 
